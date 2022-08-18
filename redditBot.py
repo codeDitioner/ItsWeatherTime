bb93253dc698d0413c3270385c2c3251547c2b0f
codeDitioner <100062754+codeDitioner@users.noreply.github.com>
2022-08-17 20:58:37 -0700

    Code Updates

--- redditBot.py
+++ redditBot.py
@@ -1,8 +1,13 @@
-# Reddit Bot that posts weather forecast for Seattle area.
+# Reddit Bot that posts weather forecast for Seattle area in SeattleWeatherTime subreddit.
 import praw
 from itsWeatherTime import weather_today
 
-reddit = praw.Reddit("bot")
-subreddit = reddit.subreddit('SeattleWeatherTime')
-for submission in subreddit.new():
+def reddit_post(arg=None): # Cloud will not run without a default argument
+    """
+    Function posts to threads with today's weather information for Seattle.
+    """
+    # Initiate instance with "bot" profile from praw.ini file.
+    reddit = praw.Reddit("bot")
+    # Assign subreddit location
+    submission = reddit.submission(url='https://www.reddit.com/r/SeattleWeatherTime/comments/wqbz04/daily_weather_updates/')
     submission.reply(body=weather_today('Seattle'))
